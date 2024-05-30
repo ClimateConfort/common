0c71f530849a7ef1be96bbc03ea9466e785c9b52
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-05-30 12:38:52 +0200

    Add constants

--- src/main/java/com/climateconfort/common/Constants.java
+++ src/main/java/com/climateconfort/common/Constants.java
@@ -2,4 +2,5 @@ package com.climateconfort.common;
 
 public class Constants {
     public static final String SENSOR_EXCHANGE_NAME = "sensor-data";
+    public static final String SENSOR_ACTION_EXCHANGE = "sensor-action";
 }
