7a463410dba969757390e80240b1471f0989c8bf
Diogo_S <97432225+DiosGod@users.noreply.github.com>
2024-06-08 20:15:21 +0200

    Update Constants.java

--- src/main/java/com/climateconfort/common/Constants.java
+++ src/main/java/com/climateconfort/common/Constants.java
@@ -3,4 +3,5 @@ package com.climateconfort.common;
 public class Constants {
     public static final String SENSOR_EXCHANGE_NAME = "sensor-data";
     public static final String SENSOR_ACTION_EXCHANGE = "sensor-action";
+    public static final String HEARTBEAT_EXCHANGE = "heartbeat";
 }
